13333f59c6de7754100bc30c35c8bbb1eb829c68
Giannis Tsagatakis <tsagatakis@protonmail.com>
2017-02-27 01:55:29 +0200

    Fix livereload

--- scripts/jekyll_server.sh
+++ scripts/jekyll_server.sh
@@ -6,7 +6,7 @@ fuser -k 4000/tcp
 mkdir -p /vagrant/tmp/run/  /vagrant/tmp/log
 
 # We use a plugin so no need for --force_polling
-bundle exec jekyll serve  --watch \
+LISTEN_GEM_DEBUGGING=1 bundle exec jekyll serve  --watch \
     --destination /vagrant/_site \
     --livereload \
     --host 0.0.0.0
